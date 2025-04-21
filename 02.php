fa0fd6cdf48d531450b5451ccec1d6c6edd54509
henry <henry_tene@hotmail.com>
2025-04-21 17:43:32 -0500

    :sparkles: Instanciando una clase

--- 02.php
+++ 02.php
@@ -1,3 +1,22 @@
 <?php
 include 'includes/header.php';
 
+//Instanciar una clase
+
+class User{
+
+}
+
+$user1 = new User();
+$user2 = new User();
+$user3 = new User();
+
+echo "<pre>";
+var_dump($user1);
+echo "</pre>";
+echo "<pre>";
+var_dump($user2);
+echo "</pre>";
+echo "<pre>";
+var_dump($user3);
+echo "</pre>";
