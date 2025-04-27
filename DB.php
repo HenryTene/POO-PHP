21903b65c71ab9dfc153970b9596e50c73b35bf1
henry <henry_tene@hotmail.com>
2025-04-26 22:38:03 -0500

    :sparkles: Se renombre una variable en la clase BD

--- DB.php
+++ DB.php
@@ -1,20 +1,21 @@
 <?php
 
-
+// Nombre de la clase: DB
+// Descripción: Esta clase representa una base de datos y tiene un método para guardar datos en ella.
 class DB
 {
-    protected $nombre;
+    protected $name;
 
     public function __construct($nombre)
     {
-        $this->nombre = $nombre;
+        $this->name = $nombre;
         
     }
 
 
     public function guardar(){
 
-        echo "Guardando en la base de datos... ".$this->nombre;
+        echo "Guardando en la Base de Datos... ".$this->name;
     }
     
 }
